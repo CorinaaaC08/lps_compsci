f929923cc03ba33d93481100c6d3cf326643d919
Corina Condori <rc.condori.corina@leadps.org>
2017-04-28 05:04:12 +0000

    HTML File

---
+++ class_samples/6-4WritingHTML/webpage.py
@@ -0,0 +1,21 @@
+<html>
+<head> 
+<title> Travel Booking! </title> 
+
+<style>
+</style> 
+
+</head>
+ <body>
+  
+  <h1>Welcome to travel booking!</h1>
+  <img src = "https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/640/360/75/wdpromedia.disney.go.com/media/wdpro-assets/dlr/gallery/destinations/disneyland-park/disneyland-gallery00.jpg?18042014104654" />
+   <p> Choose one of the most famous attractions in California. <br /> The most happiest place on earth! </p>
+<p> Here is a link to get more information. </p>
+<a href="https://disneyland.disney.go.com" target ="_blank"> Disneyland </a>
+<h2> Our most popular location is in Orlando, Florida </h2>
+<img src = "http://doubletree3.hilton.com/resources/media/dt/MCOUNDT/en_US/img/shared/full_page_image_gallery/main/DT_ustudiosaux2_2_746x420_FitToBoxSmallDimension_Center.jpg" />
+<p> Here in Universal Studios you will be able to enjoy the rides while also being able to get the 3-D experience! </p> 
+<a href =" https://www.universalorlando.com/web/en/us/index.html" target = "_blank" > Learn more. </a> 
+</body>
+</html>
